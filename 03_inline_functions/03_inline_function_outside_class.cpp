a3749dc433cac251ec750140071bc95a5fa6a60b
Roshan Mallick <roshanmallick2025@gmail.com>
2026-08-28 19:48:28 +0530

    Add inline function outside class example0

---
+++ 03_inline_functions/03_inline_function_outside_class.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class test {
+
+
+	public : void display (); // Function declared inside the class
+
+};
+
+inline void test :: display () {
+    
+	cout << "This is an inline function outside the class" << endl ;
+
+}
+
+
+int main  () {
+
+	test t1;
+	t1.display();
+
+
+
+ return 0;
+}
