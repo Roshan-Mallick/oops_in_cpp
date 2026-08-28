f1bd7476a344176e5dc1684af5071e56bba4ca18
Roshan Mallick <roshanmallick2025@gmail.com>
2026-08-28 13:04:13 +0530

    feat: add member function defined inside class

---
+++ 02_member_functions/01_functions_inside_class.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+class test {
+
+ 
+	public : void display() {
+
+		   cout << "Function declared and defined inside the class." << endl;
+                   cout << "Function is under the public access specifier." << endl;
+		
+		 }
+
+};
+
+int main () {
+
+
+
+ return 0;
+}
